8502f81c3feaf6b2db8cb877f49b956fc8bd93e2
KyleTheUndead2 <85130509+KyleTheUndead2@users.noreply.github.com>
2021-12-14 17:38:25 +0100

    add fullscreen

--- JS/clock.js
+++ JS/clock.js
@@ -1,67 +1,98 @@
 function Time() {
-    // Creating object of the Date class
-    var date = new Date();
-    // Get current hour
-    var hour = date.getHours();
-    // Get current minute
-    var minute = date.getMinutes();
-    // Get current second
-    var second = date.getSeconds();
-    // Variable to store AM / PM
-    var period = "";
-    // Assigning AM / PM according to the current hour
-    if (hour >= 12) {
+  // Creating object of the Date class
+  var date = new Date();
+  // Get current hour
+  var hour = date.getHours();
+  // Get current minute
+  var minute = date.getMinutes();
+  // Get current second
+  var second = date.getSeconds();
+  // Variable to store AM / PM
+  var period = "";
+  // Assigning AM / PM according to the current hour
+  if (hour >= 12) {
     period = "PM";
-    } else {
+  } else {
     period = "AM";
-    }
-    // Converting the hour in 12-hour format
-    if (hour == 0) {
+  }
+  // Converting the hour in 12-hour format
+  if (hour == 0) {
     hour = 12;
-    } else {
+  } else {
     if (hour > 12) {
-    hour = hour - 12;
+      hour = hour - 12;
     }
+  }
+  // Updating hour, minute, and second
+  // if they are less than 10
+  hour = update(hour);
+  minute = update(minute);
+  second = update(second);
+  // Adding time elements to the div
+  document.getElementById("digital-clock").innerText = hour + ":" + minute + ":" + second + " " + period;
+  // Set Timer to 1 sec (1000 ms)
+  setTimeout(Time, 1000);
+}
+function Time2() {
+  // Creating object of the Date class
+  var date2 = new Date();
+  // Get current hour
+  var hour2 = date2.getHours();
+  // Get current minute
+  var minute2 = date2.getMinutes();
+  // Get current second
+  var second2 = date2.getSeconds();
+  // Variable to store AM / PM
+  var period2 = "";
+  // Assigning AM / PM according to the current hour
+  if (hour2 >= 12) {
+    period2 = "PM";
+  } else {
+    period2 = "AM";
+  }
+  // Converting the hour in 12-hour format
+  if (hour2 == 0) {
+    hour2 = 12;
+  } else {
+    if (hour2 > 12) {
+      hour2 = hour2 - 12;
     }
-    // Updating hour, minute, and second
-    // if they are less than 10
-    hour = update(hour);
-    minute = update(minute);
-    second = update(second);
-    // Adding time elements to the div
-    document.getElementById("digital-clock").innerText = hour + ":" + minute + ":" + second + " " + period;
-    // Set Timer to 1 sec (1000 ms)
-    setTimeout(Time, 1000);
-   }
-    // Function to update time elements if they are less than 10
-    // Append 0 before time elements if they are less than 10
-   function update(t) {
-    if (t < 10) {
+  }
+  // Updating hour, minute, and second
+  // if they are less than 10
+  hour2 = update(hour2);
+  minute2 = update(minute2);
+  second2 = update(second2);
+  // Adding time elements to the div
+  document.getElementById("digital-clock2").innerText = hour2 + ":" + minute2 + ":" + second2 + " " + period2;
+  // Set Timer to 1 sec (1000 ms)
+  setTimeout(Time2, 1000);
+}
+// Function to update time elements if they are less than 10
+// Append 0 before time elements if they are less than 10
+function update(t) {
+  if (t < 10) {
     return "0" + t;
-    }
-    else {
+  }
+  else {
     return t;
-    }
-   }
-   Time();
-
-var elem = document.documentElement;
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
   }
 }
-
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) { /* Safari */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE11 */
-    document.msExitFullscreen();
+Time();
+function update(t) {
+  if (t < 10) {
+    return "0" + t;
   }
+  else {
+    return t;
+  }
+}
+Time2();
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 }
